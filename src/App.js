8d0d8669d823ffb2a87ae848d6e6fbc5a286190c
Antonio <antoniollado222@gmail.com>
2024-11-10 18:09:26 -0300

    paginas lobby y multipleslobbies (cositas a terminar despues)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
-/// import MasterPage from './pages/masterPage';
- import Ajustes from './pages/Ajustes';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
- import Jugar from './pages/Jugar';
- import PaginaCajero from './pages/PaginaCajero';
- import PaginaRuleta from './pages/PaginaRuleta';
- import Preguntas from './pages/Preguntas';
+// Importa tus páginas
+import MasterPage from './pages/masterPage';
+import Ajustes from './pages/Ajustes';
+import Jugar from './pages/Jugar';
+import PaginaCajero from './pages/PaginaCajero';
+import PaginaRuleta from './pages/PaginaRuleta';
+import Preguntas from './pages/Preguntas';
 import PrivateRoute from './pages/PrivateRoute';
 import IniciarSesion from './pages/IniciarSesion';
 import PaginaRegistro from './pages/Registro';
-import { BrowserRouter as Router, Routes, Route,Navigate  } from 'react-router-dom';
+import Lobby from './pages/Lobby';
+import MultiplesLobbies from './pages/MultiplesLobbies';
 
-function HomePage() {
+function App() {
   const username = localStorage.getItem('username');
 
   return (
     <Router>
       <Routes>
-        <Route 
-          path="/" 
-          element={username ? <Navigate to="/jugar" /> : <Navigate to="/login" />} 
+        {/* Redirección en la raíz dependiendo del estado de sesión */}
+        <Route
+          path="/"
+          element={username ? <Navigate to="/jugar" /> : <Navigate to="/login" />}
         />
+
+        {/* Rutas de acceso público */}
         <Route path="/login" element={<IniciarSesion />} />
         <Route path="/register" element={<PaginaRegistro />} />
+
+        {/* Rutas privadas */}
         <Route path="/paginaRuleta" element={<PrivateRoute><PaginaRuleta /></PrivateRoute>} />
         <Route path="/ajustes" element={<PrivateRoute><Ajustes /></PrivateRoute>} />
         <Route path="/preguntas" element={<PrivateRoute><Preguntas /></PrivateRoute>} />
         <Route path="/cajero" element={<PrivateRoute><PaginaCajero /></PrivateRoute>} />
         <Route path="/jugar" element={<PrivateRoute><Jugar /></PrivateRoute>} />
+        <Route path="/lobby" element={<PrivateRoute><Lobby /></PrivateRoute>} />
+        <Route path="/multipleslobbies" element={<PrivateRoute><MultiplesLobbies /></PrivateRoute>} />
       </Routes>
     </Router>
   );
 }
 
-export default HomePage;
+export default App;
